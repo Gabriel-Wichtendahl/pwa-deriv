20ffed61736aa2813223da812152c60368279c7b
Gabriel-Wichtendahl <118149051+Gabriel-Wichtendahl@users.noreply.github.com>
2026-02-01 18:37:51 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,27 @@
-self.addEventListener("install", e => {
-  self.skipWaiting();
+
+const CACHE_NAME = "deriv-pwa-v1";
+
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./app.js",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png",
+  "./alert.mp3"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
 });
 
-self.addEventListener("fetch", () => {});
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
 
