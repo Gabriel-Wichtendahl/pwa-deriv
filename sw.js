a59d019931d7b9bf196f2785116071fffca7f0e0
Gabriel-Wichtendahl <118149051+Gabriel-Wichtendahl@users.noreply.github.com>
2026-02-02 07:42:26 -0300

    Actualizar sw.js

--- sw.js
+++ sw.js
@@ -1,27 +1,11 @@
-
-const CACHE_NAME = "deriv-pwa-v1";
-
-const ASSETS = [
-  "./",
-  "./index.html",
-  "./app.js",
-  "./manifest.json",
-  "./icon-192.png",
-  "./icon-512.png",
-  "./alert.mp3"
-];
-
 self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
-  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(self.clients.claim());
 });
 
 self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
+  // passthrough – no cacheamos nada
 });
-
